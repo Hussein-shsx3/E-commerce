2e11970ad268db5c3c4a99b396ebb75c7f893182
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-10-11 17:43:01 +0300

    edit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { errorHandler } from "./middleware/errorMiddleware.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 import registerRoute from "./routes/register.js";
 import authRoute from "./routes/auth.js";
@@ -29,6 +29,7 @@ app.use("/api/product", productsRoute);
 app.use("/api/orders", orderRoute);
 
 app.use(errorHandler);
+app.use(notFound);
 
 mongoose
   .connect(process.env.MONGODB)
--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,4 +1,9 @@
-
+// middleware/notFound.js
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 // Error handling middleware
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
@@ -9,4 +14,4 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { errorHandler };
+export { errorHandler, notFound };
