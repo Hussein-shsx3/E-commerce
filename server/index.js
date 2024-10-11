d313d87c96e59dbd7733b3ae2b081dc96aa551ea
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-10-11 17:38:18 +0300

    edit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 import registerRoute from "./routes/register.js";
 import authRoute from "./routes/auth.js";
