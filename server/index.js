5e95ee04c0b242c206414ed213b7d2171604a252
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-10-12 12:01:00 +0300

    edit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "e-commerce-frontend-six-sand.vercel.app",
+      "https://e-commerce-black-alpha-68.vercel.app",
     ],
     credentials: true,
   })
